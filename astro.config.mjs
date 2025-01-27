7de6ce8eca7da51221b351416b532e335d90db24
Fabrizio Contu <fab.contu@gmail>
2025-01-27 13:43:57 +0100

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import solid from "@astrojs/solid-js";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), solid()],
 });
